b08311bdd2e6424c0e7fe5f650a8c94ff37eea9c
Meetsoni2074 <157230570+Meetsoni2074@users.noreply.github.com>
2026-02-19 23:24:54 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,22 @@
+# Day 1
+
+# print("Hello Word")#q1
+
+# name = input("enter your name:")
+# print(f"My name is: {name}")#q2
+
+# n = int(input("enter a no:"))#q3
+
+# float_convert = float(n)
+# print(float_convert)#4
+
+# a = 10
+# b = 5
+
+# print(a+b)
+# print(a-b)
+# print(a//b)#5
+
+# print("hello " * 3)#7
+
+
